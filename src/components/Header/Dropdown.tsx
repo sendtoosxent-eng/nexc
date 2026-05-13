6cb39736d13582eeb58e607f13e22cc1a9e46745
sendtoosxent-eng <sendtoosxent@gmail.com>
2026-05-13 10:47:07 +0300

    project upload

--- src/components/Header/Dropdown.tsx
+++ src/components/Header/Dropdown.tsx
@@ -9,15 +9,15 @@ const Dropdown = ({ menuItem, stickyMenu }) => {
   return (
     <li
       onClick={() => setDropdownToggler(!dropdownToggler)}
-      className={`group relative before:w-0 before:h-[3px] before:bg-blue before:absolute before:left-0 before:top-0 before:rounded-b-[3px] before:ease-out before:duration-200 hover:before:w-full ${
+      className={`group relative before:w-0 before:h-[3px] before:bg-pacific before:absolute before:left-0 before:top-0 before:rounded-b-[3px] before:ease-out before:duration-200 hover:before:w-full ${
         pathUrl.includes(menuItem.title) && "before:!w-full"
       }`}
     >
       <a
         href="#"
-        className={`hover:text-blue text-custom-sm font-medium text-dark flex items-center gap-1.5 capitalize ${
+        className={`hover:text-pacific text-custom-sm font-medium text-dark flex items-center gap-1.5 capitalize ${
           stickyMenu ? "xl:py-4" : "xl:py-6"
-        } ${pathUrl.includes(menuItem.title) && "!text-blue"}`}
+        } ${pathUrl.includes(menuItem.title) && "!text-pacific"}`}
       >
         {menuItem.title}
         <svg
@@ -49,8 +49,8 @@ const Dropdown = ({ menuItem, stickyMenu }) => {
           <li key={i}>
             <Link
               href={item.path}
-              className={`flex text-custom-sm hover:text-blue hover:bg-gray-1 py-[7px] px-4.5 ${
-                pathUrl === item.path && "text-blue bg-gray-1"
+              className={`flex text-custom-sm hover:text-pacific hover:bg-gray-1 py-[7px] px-4.5 ${
+                pathUrl === item.path && "text-pacific bg-gray-1"
               } `}
             >
               {item.title}
